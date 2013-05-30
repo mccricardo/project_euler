740be0db4c45923eedb79f70188221d5a1539179
mccricardo <mcc.ricardo@gmail.com>
2013-05-30 15:59:17 +0100

    First correct solution.

--- problem_41/python/problem41.py
+++ problem_41/python/problem41.py
@@ -8,6 +8,7 @@
 # Stop as soon as the first one is found
 
 import itertools
+from math import sqrt
 
 def tupleToNumber(tuple_number):
 	number = 0
@@ -18,8 +19,16 @@ def tupleToNumber(tuple_number):
 
 	return number
 
+# Naive (brute-force) implementation for prime checking.
+# Because the biggest possible number is 987654321, and it's sqrt approximately 31426
+# this aproach is not problematic.
+# For more efficient solutions, you can check the link below for guidance:
+#	- http://en.wikipedia.org/wiki/Primality_test
 def isPrime(number):
-	pass
+	for i in xrange(2, int(sqrt(number))):
+		if number % i == 0:
+			return False
+	return True
 
 digits = 0
 possible_digits = [9,8,7,6,5,4,3,2,1]
@@ -28,11 +37,6 @@ while digits < len(possible_digits):
 	# Also, numbers are already sorted.
 	for i in itertools.permutations(possible_digits[digits:]):		
 		if isPrime(tupleToNumber(i)):
-			print tupleToNumber(i), " is the biggest possible prime."
-		
+			print tupleToNumber(i), "is the biggest possible prime."
+			exit(0)
 	digits += 1
-
-
-
-
-
