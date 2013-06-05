342fcfda1a1ecd81517e739021aaf2a193a8e056
mccricardo <mcc.ricardo@gmail.com>
2013-06-05 10:09:14 +0100

    Tcl: check if number is pentagonal.

---
+++ problem_44/tcl/problem44.tcl
@@ -0,0 +1,30 @@
+# From the problem: Pn = n(3n - 1) / 2
+# Using a little math we get to:
+#	3n**2 - n - 2Pn = 0
+#
+# To find if a number is Pentagonal we can solve that equation 
+# using the quadratic form: 
+#
+# n = (-b +/- sqrt(b**2 - 4*a*c)) / 2*a
+# where a = 3(coefficient of 3n**2), b = -1(coefficient of n), and c = -2*tn.
+#
+# Making the appropriate substitutions, we get:
+#	n = (-(-1) +/- sqrt((-1)**2 - 4 * 3 * (-2) * Pn)) / 2 * 3
+#
+# Simplifying:
+#	n = (1 +/- sqrt(1 + 24 * Pn)) / 6
+#
+# Because we don't want a negative number, we can rule out the "-" before the 
+# sqrt, and so we get:
+#	n = (1 + sqrt(1 + 24 * Pn)) / 6 
+
+# Check if word is triangle, using the above formula for that.
+proc number_is_pentagonal {number} {
+	set n [expr {(1 + sqrt(1 + 24 * $number)) / 2}]
+	# Check if result is integer, and if so, we have a pentagonal number
+	return [expr {$n == floor($n)}]	
+}
+
+
+
+
