978244e530df0d77c0bd1bf643dab85af9900848
mccricardo <mcc.ricardo@gmail.com>
2013-06-12 11:41:01 +0100

    Solution using the Sieve of Atkins.

--- problem_47/python/problem47.py
+++ problem_47/python/problem47.py
@@ -5,29 +5,32 @@
 #	- if not, set to 0
 #	- stop when we have 4
 
+from sieve_of_atkin import SieveOfAtkin
+
 # We don't need the actual factorization, only the number of factors
 def distinct_prime_factors(number):
 	distinct_prime_factors = 0
-	for p in primes_list:		
+	for p in primes:		
 		if number % p == 0:
 			distinct_prime_factors += 1
+			number /= p
+
 	return distinct_prime_factors
 
 number_of_distinc_primes = 0
 number = 2
-primes_list = set()
-while number_of_distinc_primes != 4 :
-	# If it's prime, no need to try it
-	if all(number % p for p in primes_list):
-		primes_list.add(number)
-		number_of_distinc_primes = 0
-	else:		
-		# If number of distinct primes is 4, count it
-		if distinct_prime_factors(number) == 4:				
-			number_of_distinc_primes += 1
-		# If not, reset to 0
-		else:
-			number_of_distinc_primes = 0		
+
+# For this specfic of the problem, we only need primes up to 700.
+# To a more generic number factorization, we would need primes up to number/2.
+soa = SieveOfAtkin(700)
+primes = set(soa.getPrimes())
+while number_of_distinc_primes != 4 :	
+	# If number of distinct primes is 4, count it
+	if distinct_prime_factors(number) == 4:				
+		number_of_distinc_primes += 1
+	# If not, reset to 0
+	else:
+		number_of_distinc_primes = 0		
 	number += 1	
 
 print "The first number is:", number - 4
