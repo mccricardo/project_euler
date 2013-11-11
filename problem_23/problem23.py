48b0c2bafbf5c0cb38784202b10225d1231853cb
mccricardo <mcc.ricardo@gmail.com>
2013-11-11 15:06:16 +0000

    Moved from range to xrange.

--- problem_23/problem23.py
+++ problem_23/problem23.py
@@ -3,7 +3,7 @@ abundant_numbers = set()
 # 4179871
 
 def get_divisors(number):
-	return [x for x in range(1, number/2 +1) if number % x == 0]
+	return [x for x in xrange(1, number/2 +1) if number % x == 0]
 
 for i in range(1,  28123):
 	if sum(get_divisors(i)) > i: 
